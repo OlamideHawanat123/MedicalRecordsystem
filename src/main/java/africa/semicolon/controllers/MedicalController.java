444d43edab4f8c288a57f9e5bf67b7f7b6b45435
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-08-23 18:43:12 +0100

    pending Doctor Controller Added

--- src/main/java/africa/semicolon/controllers/MedicalController.java
+++ src/main/java/africa/semicolon/controllers/MedicalController.java
@@ -1,4 +1,29 @@
 package africa.semicolon.controllers;
 
+import africa.semicolon.data.models.Doctors;
+import africa.semicolon.data.repositories.PendingDoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medical")
 public class MedicalController {
+    @Autowired
+    private PendingDoctorRepository pendingDoctorRepository;
+
+    @GetMapping("/pendingDoctors")
+    public ResponseEntity<?> getPendingDoctors (){
+        try {
+            List<Doctors> pendingDoctors = pendingDoctorRepository.findAll();
+            return ResponseEntity.status(HttpStatus.FOUND).body(pendingDoctors);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
